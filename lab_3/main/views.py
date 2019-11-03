531b71be2cc9072679180ed098528307e22c1f39
Marianruzhyrskyi <marik22832@gmail.com>
2019-11-03 18:40:44 +0200

    Lab_3: Виконано всі пункти

--- lab_3/main/views.py
+++ lab_3/main/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    response = {
+            'date': (datetime.now()).strftime("%H:%M:%S %Y-%m-%d"),
+            'current_page': request.path,
+            'server_info': os.uname(),
+            'client_info': request.META['HTTP_USER_AGENT']
+    }
+    return JsonResponse(response)
+
---
+++ lab_3/monitoring.py
@@ -0,0 +1,40 @@
+import requests
+import json
+import logging
+import time
+
+logging.basicConfig(
+    filename="server.log",
+    filemode='a',
+    level=logging.INFO,
+    format='{levelname} {asctime} {name} : {message}',
+    style='{'
+)
+log = logging.getLogger(__name__)
+
+
+def main(url):
+    try:
+        r = requests.get(url)
+    except requests.HTTPError as http_err:
+        logging.error("HTTP помилка: : %s", http_err)
+        print("HTTP-помилка: \n", http_err)
+    except Exception as err:
+        logging.error("Якась помилка: : %s", err)
+        print("Виникла помилка: \n", err)
+    else:
+        if r.status_code == 200:
+            data = json.loads(r.content)
+            logging.info("Сервер доступний. Час на сервері: %s", data['date'])
+            logging.info("Запитувана сторінка: : %s", data['current_page'])
+            logging.info("Відповідь сервера місти наступні поля:")
+            for key in data.keys():
+                logging.info("Ключ: %s, Значення: %s", key, data[key])
+            print("Everythings good!")
+
+if __name__ == '__main__':
+    while True:
+        main("http://localhost:8000/health")
+        time.sleep(60)
+
+
